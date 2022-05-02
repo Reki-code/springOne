16530c6a7c5abb2c1201a056e93b4d2a9bd2e168
Yuuta <darkmasterwei@gmail.com>
2022-05-02 10:40:19 +0000

    use jdbc template

---
+++ taco-cloud/src/main/java/me/rekii/tacocloud/IngredientRepository.java
@@ -0,0 +1,12 @@
+package me.rekii.tacocloud;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IngredientRepository {
+    List<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
--- taco-cloud/src/main/java/me/rekii/tacocloud/TacoOrder.java
+++ taco-cloud/src/main/java/me/rekii/tacocloud/TacoOrder.java
@@ -1,6 +1,8 @@
 package me.rekii.tacocloud;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.Digits;
@@ -12,7 +14,10 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private Date placedAt;
     @NotBlank(message="Delivery name is required")
     private String deliveryName;
     @NotBlank(message="Street is required")
