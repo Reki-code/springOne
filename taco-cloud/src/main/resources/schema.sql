4997ef7db0ff73b75d70b5b92f13c52632491dcc
Yuuta <darkmasterwei@gmail.com>
2022-05-10 02:33:05 +0000

    fix foregin key

--- taco-cloud/src/main/resources/schema.sql
+++ taco-cloud/src/main/resources/schema.sql
@@ -27,5 +27,4 @@ create table if not exists Ingredient (
 
 alter table Taco
     add foreign key (taco_order) references Taco_Order(id);
-alter table Ingredient_Ref
-    add foreign key (ingredient) references Ingredient(id);
+
