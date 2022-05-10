81ebbed60e66de36440975cd9e110d8697a8ab4d
Yuuta <darkmasterwei@gmail.com>
2022-05-10 10:28:20 +0000

    add spring data rest

--- taco-cloud/src/main/java/me/rekii/tacocloud/data/TacoRepository.java
+++ taco-cloud/src/main/java/me/rekii/tacocloud/data/TacoRepository.java
@@ -1,9 +1,10 @@
 package me.rekii.tacocloud.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import me.rekii.tacocloud.Taco;
 
 public interface TacoRepository extends CrudRepository<Taco, Long> {
-
+    Iterable<Taco> findAll(Pageable page);
 }
